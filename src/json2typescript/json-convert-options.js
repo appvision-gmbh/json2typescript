cacb40cbd80285e038c58406a2cab704ededfc5c
Franklin Davenport <sparebytes@gmail.com>
2017-09-20 13:55:50 -0500

    v1.0.6

--- src/json2typescript/json-convert-options.js
+++ src/json2typescript/json-convert-options.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Settings = (function () {
     function Settings() {
     }
+    Settings.MAPPING_PROPERTY = "__jsonconvert__mapping__";
+    Settings.MAPPER_PROPERTY = "__jsonconvert__mapper__";
     return Settings;
 }());
-Settings.MAPPING_PROPERTY = "__jsonconvert__mapping__";
-Settings.MAPPER_PROPERTY = "__jsonconvert__mapper__";
 exports.Settings = Settings;
 ;
 var MappingOptions = (function () {
